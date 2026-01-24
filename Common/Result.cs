1ae87b2c9bf99c71b72d060c253d1a6c7f4e61a8
Windblade <juliangarzon867@gmail.com>
2026-01-24 16:13:52 -0500

    Add: Introducción del patrón result

---
+++ Common/Result.cs
@@ -0,0 +1,21 @@
+namespace PlanificadorDeHorarios.Api.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string Error { get; }
+        public bool IsFailure = !IsSuccess;
+
+
+        private Result(bool success, T? value, string error)
+        {
+            IsSuccess = success;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, string.Empty);
+        public static Result<T> Failure(string error) => new(false, default, error);
+    }
+}
