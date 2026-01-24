9b8fc9841fe979e6236d321351135fc79738b51f
Windblade <juliangarzon867@gmail.com>
2026-01-24 16:17:22 -0500

    Feat: Login

---
+++ Features/Login.cs
@@ -0,0 +1,46 @@
+﻿using PlanificadorDeHorarios.Api.Common;
+using PlanificadorDeHorarios.Api.Domain;
+
+namespace PlanificadorDeHorarios.Api.Features
+{
+    public record LoginRequest(string email, string password);
+    public class LoginEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/login", (LoginRequest request, LoginHandler handler)=>
+            {
+                var result = handler.Handle(request);
+
+                return result.IsSuccess ? Results.Ok(new { token = result.Value }) : Results.Unauthorized();
+            });
+        }
+    }
+
+    public class LoginHandler : IHandler
+    { 
+        private IUsuarioRepositorio _repositorio;
+        private IPasswordHelper _passwordHelper;
+        private ITokenGenerator _tokenGenerator;
+
+        public LoginHandler(IUsuarioRepositorio repositorio, IPasswordHelper passwordHelper, ITokenGenerator tokenGenerator)
+        {
+            _repositorio = repositorio;
+            _passwordHelper = passwordHelper;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        public Result<string> Handle(LoginRequest request)
+        {
+            Usuario usuario = _repositorio.BuscarPorEmail(request.email);
+
+            if (usuario == null)
+                return Result<string>.Failure("Usuario no existe");
+
+            if (!_passwordHelper.VerificarPassword(request.password, usuario.HashPassword))
+                return Result<string>.Failure("Contraseña incorrecta");
+
+            return Result<string>.Success(_tokenGenerator.GenerarToken(usuario));
+        }
+    }
+}
