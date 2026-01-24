d9f402bed9aafda58e565397740940ea0f5d7a8d
Windblade <juliangarzon867@gmail.com>
2026-01-24 16:28:15 -0500

    Refactor: Separación del handler y controller en registrar usuario

--- Features/RegistrarUsuario.cs
+++ Features/RegistrarUsuario.cs
@@ -1,29 +1,47 @@
-﻿using PlanificadorDeHorarios.Api.Domain;
-using PlanificadorDeHorarios.Api.Common;
+﻿using PlanificadorDeHorarios.Api.Common;
+using PlanificadorDeHorarios.Api.Domain;
 
 namespace PlanificadorDeHorarios.Api.Features
 {
-    record RegistrarUsuarioRequest(string nombre, string email, string password);
+    public record RegistrarUsuarioRequest(string nombre, string email, string password);
 
     public class RegistrarUsuarioEndpoint : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
         {
-            app.MapGet("/register", (RegistrarUsuarioRequest request, IUsuarioRepositorio usuarioRepositorio, IPasswordHelper passwordHelper) =>
+            app.MapGet("/register", (RegistrarUsuarioRequest request, RegistrarUsuarioHandler handler) =>
             {
-                if (usuarioRepositorio.VerificarSiExistePorEmail(request.email))
-                    return Results.Conflict(new{message = "El usuario ya existe"});
-
-                string password = passwordHelper.HashearPassword(request.password);
-                Usuario usuario = new()
-                {
-                    Nombre = request.nombre,
-                    Email = request.email,
-                    HashPassword = password
-                };
-                usuarioRepositorio.AgregarUsuario(usuario);
-                return Results.Ok();
+                handler.Handle(request);
             });
+        }
+    }
+
+    public class RegistrarUsuarioHandler 
+    {
+        private IUsuarioRepositorio _repositorio;
+        private IPasswordHelper _passwordHelper;
+
+        public RegistrarUsuarioHandler(IUsuarioRepositorio repositorio, IPasswordHelper passwordHelper)
+        {
+            _repositorio = repositorio;
+            _passwordHelper = passwordHelper;
+        }
+
+        public Result<string> Handle(RegistrarUsuarioRequest request) 
+        {
+            if (_repositorio.VerificarSiExistePorEmail(request.email))
+                return Result<string>.Failure("El usuario ya se encuentra registrado");
+
+            string password = _passwordHelper.HashearPassword(request.password);
+            Usuario usuario = new()
+            {
+                Nombre = request.nombre,
+                Email = request.email,
+                HashPassword = password
+            };
+            _repositorio.AgregarUsuario(usuario);
+
+            return Result<string>.Success("Usuario registrado exitosamente");
         }
     }
 }
