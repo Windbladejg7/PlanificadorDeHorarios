8ccf261579ca446943c98134452eb80e5665fdf3
Windblade <juliangarzon867@gmail.com>
2026-01-24 16:14:48 -0500

    Refactor: Ajuste a registrar usuario con result

--- Features/RegistrarUsuario.cs
+++ Features/RegistrarUsuario.cs
@@ -1,24 +1,18 @@
 ﻿using PlanificadorDeHorarios.Api.Domain;
 using PlanificadorDeHorarios.Api.Common;
-using Microsoft.AspNetCore.Identity;
 
 namespace PlanificadorDeHorarios.Api.Features
 {
-    public record RegistrarUsuarioRequest
-    {
-        public required string nombre;
-        public required string email;
-        public required string password;
-    }
+    record RegistrarUsuarioRequest(string nombre, string email, string password);
 
     public class RegistrarUsuarioEndpoint : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
         {
             app.MapGet("/register", (RegistrarUsuarioRequest request, IUsuarioRepositorio usuarioRepositorio, IPasswordHelper passwordHelper) =>
-
             {
-                if (usuarioRepositorio.VerificarSiExistePorEmail(request.email)) return;
+                if (usuarioRepositorio.VerificarSiExistePorEmail(request.email))
+                    return Results.Conflict(new{message = "El usuario ya existe"});
 
                 string password = passwordHelper.HashearPassword(request.password);
                 Usuario usuario = new()
@@ -28,6 +22,7 @@ namespace PlanificadorDeHorarios.Api.Features
                     HashPassword = password
                 };
                 usuarioRepositorio.AgregarUsuario(usuario);
+                return Results.Ok();
             });
         }
     }
