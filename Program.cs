a7a8b454b323d0961c35251f99b4817d822c514a
Windblade <juliangarzon867@gmail.com>
2026-01-26 19:05:32 -0500

    Change: Incluir jwt secret

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using PlanificadorDeHorarios.Api.Extensions;
 using PlanificadorDeHorarios.Api.Infraestructure;
 using PlanificadorDeHorarios.Api.Ports;
+using PlanificadorDeHorarios.Api.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddScoped<IPasswordHelper, PasswordHelper>();
 builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
 builder.Services.AddScoped<IUsuarioRepositorio>();
 builder.Services.AddHandlers();
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 
 var app = builder.Build();
 app.Run();
